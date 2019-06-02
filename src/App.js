085ef2a9894d35216244aa6b18be2596f7e1753e
Przemek2907 <przemyslaw.zochowski91@gmail.com>
2019-06-02 22:21:45 +0200

    Navbar created with links to  Route on the webpage

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import Recipies from './pages/Recipies';
 import SingleRecipie from './pages/SingleRecipie';
 import Default from './pages/Default';
 import {BrowserRouter as Router,Route, Switch} from "react-router-dom";
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
     <main>
+      <Navbar></Navbar>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/recipies" exact component={Recipies} />
