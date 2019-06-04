cf0022f5ccd43a8a77820a2d28ca43e2d837c989
Przemek2907 <przemyslaw.zochowski91@gmail.com>
2019-06-05 01:20:19 +0200

    Searchbar component done

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,9 +2,28 @@ import React, { Component } from 'react'
 
 export default class SearchBar extends Component {
   render() {
+    const {handleChange, handleSubmit, search} = this.props;
     return (
-      <div>
-        Hello from the SearchBar
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mx-auto col-md-8 mt-5 text-center">
+            <h1 className="text-slanted text-capitalize">
+              search recipies for <strong className="text-red"> Aga&Przemek</strong>
+            </h1>
+            <form className="mt-4">
+            <label htmlFor="search" className="text-capitalize"> Search recipies separated by comma</label>
+            <div className="input-group">
+              <input type="text" name="search" className="form-control" placeholder="beef,pepper,oil,tomatoes"
+              value={search} onChange={handleChange}/>
+              <div className="input-group-append">
+                <button type="submit" className="input-group-text bg-primary text-white">
+                  <i className="fas fa-search"/>
+                </button>
+              </div>
+            </div>
+            </form>
+          </div>
+        </div>
       </div>
     )
   }
