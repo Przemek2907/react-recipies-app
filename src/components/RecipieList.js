052d9759b5e657c0bc49f0801f2c8506afddeb0b
Przemek2907 <przemyslaw.zochowski91@gmail.com>
2019-06-05 23:26:15 +0200

    Getting props from the JSON file for the RecipieList

--- src/components/RecipieList.js
+++ src/components/RecipieList.js
@@ -3,11 +3,19 @@ import Recipie from "./Recipie";
 
 export default class RecipiesList extends Component {
   render() {
+    const {recipies} = this.props;
     return (
-      <div>
-        Hello from RecipieList
-        <Recipie />
+      <React.Fragment>
+      <div className="container py-5">
+          <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+            <h1></h1>
+            <h2 className="text-slanted">Recipie List</h2>
+          </div>
+          <div className="row">
+          {recipies.map(item => (<Recipie key={item.recipe_id} recipie={item} />))}
+            </div>
       </div>
+      </React.Fragment>
     )
   }
 }
